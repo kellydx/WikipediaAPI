a87bc30743a41877fe57fc2e5b326dc75303eb92
DKelly <kelly13493@gmail.com>
2018-12-12 22:42:26 -0500

    set up API url and search term

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
+let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
+let contentUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
+
+let userInput;
 function setup() {
   // put setup code here
+  userInput = select('#userInput');
+  userInput.changed(goWiki);
+  goWiki();
 }
 
-function draw() {
-  // put drawing code here
-  ellipse(50, 50, 80, 80);
+function goWiki() {
+  let term = userInput.value();
+  let url = searchUrl + term;
+  console.log(url);
 }
